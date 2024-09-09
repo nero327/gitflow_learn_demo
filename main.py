1c21758eb98b02cb77bd97495e576f1431083c3e
= <=>
2024-09-09 11:41:12 +0800

    finish quicksort

--- main.py
+++ main.py
@@ -17,7 +17,19 @@ def bubble_sort(arr):
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
     return arr
-#testfinish
+
+
+# testfinish
+# quicksort
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x < pivot]
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quick_sort(left) + middle + quick_sort(right)
+
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
